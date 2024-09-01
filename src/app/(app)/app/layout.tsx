fc3d2d0351f40053bf871b08c54b4fae5396889d
Christian <71664957+Christian-Mar@users.noreply.github.com>
2024-09-01 12:50:17 +0200

    nested layout app details

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import AppFooter from '@/components/app-footer';
+import AppHeader from '@/components/app-header';
+import BackgroundPattern from '@/components/background-pattern';
+import React from 'react';
 
-function Layout({children}) {
-  return (
-    <div>Header {children}</div>
-  )
+function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<>
+			<BackgroundPattern />
+      <div className="flex flex-col max-w-[1050px] mx-auto px-4 min-h-screen">
+			<AppHeader />
+			{children}
+			<AppFooter /></div>
+		</>
+	);
 }
 
-export default Layout
+export default Layout;
+// this layout goes also through the rootlayout
